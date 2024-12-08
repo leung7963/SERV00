7c366ce31f66c7fcec2737d9f7fca749494539af
leung7963 <104255180+leung7963@users.noreply.github.com>
2024-12-08 20:44:18 +0800

    更新 restart.sh

---
+++ restart.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo "检查并 重启 任务"
+
+# 检查进程是否在运行
+
+echo "检查php"
+pgrep -x "php" > /dev/null
+
+
+#如果没有运行，则启动 nezha
+if [ $? -ne 0 ]; then
+    nohup ./vless/start.sh >/dev/null 2>&1 &
+    echo "运行成功php"
+fi
+
+echo "检查http"
+pgrep -x "http" > /dev/null
+
+
+if [ $? -ne 0 ]; then
+    nohup ./vless/http -c ./vless/config.json >/dev/null 2>&1 &
+    echo "http运行成功"
+fi
+
+
+
+# 检查进程是否在运行
+echo "检查node"
+pgrep -x "node" > /dev/null
+
+
+if [ $? -ne 0 ]; then
+    nohup ./vless/tunnel.sh >/dev/null 2>&1 &
+    echo "node运行成功"
+fi
