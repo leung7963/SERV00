2157b7155389013f92ee37cc127c439dc73ae16a
leung7963 <104255180+leung7963@users.noreply.github.com>
2024-12-09 15:16:11 +0800

    更新 restart.sh

--- restart.sh
+++ restart.sh
@@ -10,30 +10,23 @@ echo "检查并 重启 任务"
 
 echo "检查php"
 pgrep -x "php" > /dev/null
-
-
 if [ $? -ne 0 ]; then
-    nohup  ./site/start.sh >/dev/null 2>&1 &
+    nohup  ${FILE_PATH}/start.sh >/dev/null 2>&1 &
     echo "运行成功php"
 fi
 
+
 echo "检查http"
 pgrep -x "http" > /dev/null
-
-
 if [ $? -ne 0 ]; then
-    nohup  ./site/http -c  ./site/config.json >/dev/null 2>&1 &
+    nohup  ${FILE_PATH}/http -c  ${FILE_PATH}/config.json >/dev/null 2>&1 &
     echo "http运行成功"
 fi
 
 
-
-# 检查进程是否在运行
 echo "检查node"
 pgrep -x "node" > /dev/null
-
-
 if [ $? -ne 0 ]; then
-    nohup  ./site/tunnel.sh >/dev/null 2>&1 &
+    nohup  ${FILE_PATH}/tunnel.sh >/dev/null 2>&1 &
     echo "node运行成功"
 fi
