e2fc814b8d5c9da26576644bfe1aa8f42c066379
leung7963 <104255180+leung7963@users.noreply.github.com>
2024-12-27 22:42:46 +0800

    更新 vless.sh

--- vless.sh
+++ vless.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export UUID=${UUID:-'fc44fe6a-f083-4591-9c03-f8d61dc3907f'}
-export NEZHA_SERVER=${NEZHA_SERVER:-'nezha.jjl.cc.ua'} 
+export NEZHA_SERVER=${NEZHA_SERVER:-'nz.0662.ip-ddns.com'} 
 export NEZHA_PORT=${NEZHA_PORT:-'443'}     
 export NEZHA_KEY=${NEZHA_KEY:-''}
 export ARGO_DOMAIN=${ARGO_DOMAIN:-''}   
