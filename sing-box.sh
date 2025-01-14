51d1626bedc5b4c0284288b2f8aa99f4cbd43978
leung7963 <104255180+leung7963@users.noreply.github.com>
2025-01-15 01:50:10 +0800

    更新 sing-box.sh

--- sing-box.sh
+++ sing-box.sh
@@ -159,11 +159,11 @@ install_singbox() {
 
     # 下载sing-box,cloudflared
     [ ! -d "${work_dir}" ] && mkdir -p "${work_dir}" && chmod 777 "${work_dir}"
-    latest_version=$(curl -s "https://api.github.com/repos/SagerNet/sing-box/releases" | jq -r '[.[] | select(.prerelease==false)][0].tag_name | sub("^v"; "")')
-    curl -sLo "${work_dir}/${server_name}" "https://github.com/SagerNet/sing-box/releases/download/v${latest_version}/sing-box-${latest_version}-linux-${ARCH}.tar.gz"
+    #latest_version=$(curl -s "https://api.github.com/repos/SagerNet/sing-box/releases" | jq -r '[.[] | select(.prerelease==false)][0].tag_name | sub("^v"; "")')
+    curl -sLo "${work_dir}/${server_name}" "https://github.com/leung7963/SERV00/releases/download/vps/sing-box-liunx-amd64"
     curl -sLo "${work_dir}/qrencode" "https://github.com/eooce/test/releases/download/${ARCH}/qrencode-linux-${ARCH}"
     curl -sLo "${work_dir}/argo" "https://github.com/eooce/test/releases/download/$ARCH/bot13"
-    tar -xzvf "${work_dir}/${server_name}.tar.gz" -C "${work_dir}/" && \
+    #tar -xzvf "${work_dir}/${server_name}.tar.gz" -C "${work_dir}/" && \
     #mv "${work_dir}/sing-box-${latest_version}-linux-${ARCH}/sing-box" "${work_dir}/" && \
     #rm -rf "${work_dir}/${server_name}.tar.gz" "${work_dir}/sing-box-${latest_version}-linux-${ARCH}"
     chown root:root ${work_dir} && chmod +x ${work_dir}/${server_name} ${work_dir}/argo ${work_dir}/qrencode
