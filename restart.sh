ab016c3346e1cdfd08a2751899a2ce5e6fd8ffad
leung7963 <104255180+leung7963@users.noreply.github.com>
2025-01-30 13:07:31 +0800

    更新 restart.sh

--- restart.sh
+++ restart.sh
@@ -29,3 +29,16 @@ if [ $? -ne 0 ]; then
     nohup ${FILE_PATH}/tunnel.sh >/dev/null 2>&1 &
     #echo "node运行成功"
 fi
+
+if [ -n "$TG_TOKEN" ] && [ -n "$CHAT_ID" ]; then
+        echo "-----------发送TG通知-----------------"
+	    local message="执行成功"
+	    response=$(curl -s -X POST "https://api.telegram.org/bot$TG_TOKEN/sendMessage" -d "chat_id=$CHAT_ID" -d "text=$message")
+
+	    # 检查响应
+	    if [[ $(echo "$response" | jq -r '.ok') == "true" ]]; then
+	        echo "::info::Telegram消息发送成功: $message"
+	    else
+	        echo "::error::Telegram消息发送失败: $response"
+	    fi
+fi
